12c70bddba48904c213b1656317077ab510fefd2
irwan muhamad Faizal <37823549+irwan279@users.noreply.github.com>
2019-11-07 09:10:57 +0700

    tugas 18 java

---
+++ tugas18-2.js
@@ -0,0 +1,6 @@
+function tugas18() {
+  var kata = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.";
+  var kata1 = "The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from de Finibus Bonorum et Malorum by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.";
+  console.log(kata.concat(kata1));
+}
+tugas18();
